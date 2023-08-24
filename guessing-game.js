ae0092793b54a3064a4096cc817d8e8851704bad
ayoWhatItDo <130956867+ayoWhatItDo@users.noreply.github.com>
2023-08-23 23:44:06 -0700

    Update guessing-game.js

--- guessing-game.js
+++ guessing-game.js
@@ -1,18 +1,18 @@
 function guessingGame() {
-  const ANSWER = Math.floor(Math.random() * 100);
+  const RandNum = Math.floor(Math.random() * 100);
   let isOver = false;
   let numGuesses = 0;
 
   return function guess(num) {
     if (isOver) return "The game is over, you already won!";
     numGuesses++;
-    if (num === ANSWER) {
+    if (num === RandNum) {
       isOver = true;
       const guess = numGuesses === 1 ? "guess" : "guesses";
       return `You win! You found ${num} in ${numGuesses} ${guess}.`;
     }
-    if (num < ANSWER) return `${num} is too low!`;
-    if (num > ANSWER) return `${num} is too high!`;
+    if (num < RandNum) return `${num} is too low! try again`;
+    if (num > RandNum) return `${num} is too high!`;
   };
 }
 
