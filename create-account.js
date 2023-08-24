5f7d2af9898412d6a8d7848989072721bb61a395
ayoWhatItDo <130956867+ayoWhatItDo@users.noreply.github.com>
2023-08-23 23:11:05 -0700

    Update create-account.js

--- create-account.js
+++ create-account.js
@@ -1,20 +1,20 @@
 function createAccount(pin, amount = 0) {
   return {
     checkBalance(inputPin) {
-      if (inputPin !== pin) return "Invalid PIN.";
+      if (inputPin !== pin) return "Invalid PIN. Sike that's the wrong numbaa!";
       return `$${amount}`;
     },
     deposit(inputPin, newAmount) {
       if (inputPin !== pin) return "Invalid PIN.";
       amount += newAmount;
-      return `Succesfully deposited $${newAmount}. Current balance: $${amount}.`;
+      return `Succesfully deposited $${newAmount}. Your Current balance is ${amount}. Damn son you broke broke!`;
     },
     withdraw(inputPin, withdrawalAmount) {
       if (inputPin !== pin) return "Invalid PIN.";
       if (withdrawalAmount > amount)
         return "Withdrawal amount exceeds account balance. Transaction cancelled.";
       amount -= withdrawalAmount;
-      return `Succesfully withdrew $${withdrawalAmount}. Current balance: $${amount}.`;
+      return `Succesfully withdrew $${withdrawalAmount}. Current balance: $${amount}. Damn son you still broke broke!`;
     },
     changePin(oldPin, newPin) {
       if (oldPin !== pin) return "Invalid PIN.";
